189a7381907c229c4e76ef21ac99b16958bd4178
z80 <z80dev@mlnl.finance>
2022-04-27 17:02:57 -0400

    implement receiveAsset function

--- src/NFTYeeter.sol
+++ src/NFTYeeter.sol
@@ -1,20 +1,37 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.11;
 
 import "solmate/tokens/ERC721.sol";
+import {IConnextHandler} from "nxtp/interfaces/IConnextHandler.sol";
+import {IExecutor} from "nxtp/interfaces/IExecutor.sol";
 import "ERC721X/interfaces/IERC721X.sol";
+import "ERC721X/ERC721X.sol";
 
 contract NFTYeeter is ERC721TokenReceiver {
 
     uint32 public immutable localDomain;
     address public immutable connext;
+    address public owner;
 
     mapping(address => mapping(uint256 => DepositDetails)) deposits; // deposits[collection][tokenId] = depositor
-    mapping(uint16 => mapping(address => address)) localAddress; // localAddress[originChainId][collectionAddress]
+    mapping(uint32 => mapping(address => address)) localAddress; // localAddress[originChainId][collectionAddress]
+    mapping(uint32 => address) trustedYeeters; // remote addresses of other yeeters, though ideally
+                                               // we would want them all to have the same address. still, some may upgrade
 
     constructor(uint32 _localDomain, address _connext) {
         localDomain = _localDomain;
         connext = _connext;
+        owner = msg.sender;
+    }
+
+    function setOwner(address newOwner) external {
+        require(msg.sender == owner);
+        owner = newOwner;
+    }
+
+    function setTrustedYeeter(uint32 chainId, address yeeter) external {
+        require(msg.sender == owner);
+        trustedYeeters[chainId] = yeeter;
     }
 
     // this is maintained on each "Home" chain where an NFT is originally locked
@@ -47,6 +64,42 @@ contract NFTYeeter is ERC721TokenReceiver {
         require(details.bridged == false, "NFT Currently Bridged");
         require(details.depositor == msg.sender, "Unauth");
         ERC721(collection).safeTransferFrom(address(this), msg.sender, tokenId);
+    }
+
+    // function called by remote contract
+    // this signature maximizes future flexibility & backwards compatibility
+    function receiveAsset(bytes memory _payload) public {
+        // only connext can call this
+        require(msg.sender == connext, "NOT_CONNEXT");
+        // check remote contract is trusted remote NFTYeeter
+        uint32 remoteChainId = IExecutor(msg.sender).origin();
+        address remoteCaller = IExecutor(msg.sender).originSender();
+        require(trustedYeeters[remoteChainId] == remoteCaller, "UNAUTH");
+
+        (BridgedTokenDetails memory details) = abi.decode(_payload, (BridgedTokenDetails));
+
+        if (details.originChainId == localDomain) {
+            // we're bridging this NFT *back* home
+            DepositDetails storage depositDetails = deposits[details.originAddress][details.tokenId];
+
+            // record new owner to enable them to withdraw
+            depositDetails.depositor = details.owner;
+
+            // record that the NFT is *back* and does not exist on other chains
+            depositDetails.bridged = false;
+
+        } else if (localAddress[details.originChainId][details.originAddress] != address(0)) {
+            // local XERC721 contract exists, we just need to mint
+            ERC721X nft = ERC721X(localAddress[details.originChainId][details.originAddress]);
+            nft.mint(details.owner, details.tokenId, details.tokenURI);
+        } else {
+            // deploy new ERC721 contract
+            ERC721X nft = new ERC721X(details.name, details.symbol, details.originAddress, details.originChainId);
+            localAddress[details.originChainId][details.originAddress] = address(nft);
+            nft.mint(details.owner, details.tokenId, details.tokenURI);
+        }
+
+
     }
 
     function onERC721Received(
